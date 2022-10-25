c7d6744802a099a60c2a91d3be02a0d7540accb3
AlexAguado24 <aaguado131@gmail.com>
2022-10-25 12:17:56 +0200

    ejercicio terminado

--- EjercicioPsp/src/Hijo.java
+++ EjercicioPsp/src/Hijo.java
@@ -1,14 +1,68 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Serializable;
+import java.util.ArrayList;
 
 public class Hijo {
 
-    public static void main(String[] args) throws IOException {
+    public static class Zapatilla implements Serializable{
+
+        String marca;
+
+        public Zapatilla(String marca) {
+            this.marca = marca;
+        }
+
+        public String getMarca() {
+            return marca;
+        }
+
+        public void setMarca(String marca) {
+            this.marca = marca;
+        }
+    }
+
+    public static void main(String[] args){
+
+        ArrayList<Zapatilla> zapatillas = new ArrayList<>();
+
+        Zapatilla zapatilla = new Zapatilla("nike");
+        Zapatilla zapatilla2 = new Zapatilla("adidas");
+        Zapatilla zapatilla3 = new Zapatilla("puma");
+        Zapatilla zapatilla4 = new Zapatilla("nike");
+        Zapatilla zapatilla5 = new Zapatilla("adidas");
+        Zapatilla zapatilla6 = new Zapatilla("puma");
+        Zapatilla zapatilla7 = new Zapatilla("nike");
+        Zapatilla zapatilla8 = new Zapatilla("adidas");
+        Zapatilla zapatilla9 = new Zapatilla("puma");
+        Zapatilla zapatilla10 = new Zapatilla("nike");
+
+        zapatillas.add(zapatilla);
+        zapatillas.add(zapatilla2);
+        zapatillas.add(zapatilla3);
+        zapatillas.add(zapatilla4);
+        zapatillas.add(zapatilla5);
+        zapatillas.add(zapatilla6);
+        zapatillas.add(zapatilla7);
+        zapatillas.add(zapatilla8);
+        zapatillas.add(zapatilla9);
+        zapatillas.add(zapatilla10);
+
+        int cantidad = 0;
         String enviar;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        enviar = new String();
-        enviar =br.readLine();
-        System.out.println("Soy el hijo "+enviar);
+        try {
+            enviar = new String();
+            enviar =br.readLine();
+            for (Zapatilla item: zapatillas) {
+                if (enviar.equalsIgnoreCase(item.getMarca())) {
+                    cantidad++;
+                }
+            }
+            System.out.println("Tengo "+ cantidad);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- EjercicioPsp/src/Padre.java
+++ EjercicioPsp/src/Padre.java
@@ -7,10 +7,13 @@ public class Padre {
 
     public static void main(String[] args) throws IOException {
         String linea;
+        String tipo;
         Process hijo = new ProcessBuilder("java","C:\\Users\\Usuario DAM2\\Desktop\\DAM2\\Alexaguado_PSP\\EjercicioPsp\\src\\Hijo.java").start();
         BufferedReader br = new BufferedReader(new InputStreamReader(hijo.getInputStream()));
         PrintStream ps = new PrintStream(hijo.getOutputStream(), true);
         BufferedReader in = new BufferedReader(new InputStreamReader (System.in));
+        System.out.println("-----Hola hijo------");
+        System.out.println("");
         linea = in.readLine();
         ps.println(linea);
         linea = br.readLine();
