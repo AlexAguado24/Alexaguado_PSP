f8ea56cd91662cf838e5088e125786dc0e32ae90
AlexAguado24 <aaguado131@gmail.com>
2023-02-14 11:10:05 +0100

    .

---
+++ TemasFinales/ProyectoFilezilla/src/Comprobar.java
@@ -0,0 +1,2 @@
+public class Comprobar {
+}
---
+++ TemasFinales/ProyectoFilezilla/src/Firma.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.security.*;
+
+public class Firma{
+
+    public static void main(String args[]) {
+        try {
+            FileOutputStream fileout = new FileOutputStream("firma.dat");
+            ObjectOutputStream dataOS = new ObjectOutputStream(fileout);
+            MessageDigest md = MessageDigest.getInstance("SHA");
+            String datos = "Texto enviado";
+            byte dataBytes[] = datos.getBytes();
+            md.update(dataBytes);// TEXTO A RESUMIR
+            byte resumen[] = md.digest(); // SE CALCULA EL RESUMEN
+            dataOS.writeObject(datos); // se escriben los datos
+            dataOS.writeObject(resumen);// se escribe el resumen
+            dataOS.close();
+            fileout.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+}
