d4ace44f473fdfad6c563de3d9bbb5d939432672
AlexAguado24 <aaguado131@gmail.com>
2023-02-22 13:24:43 +0100

    .

--- TemasFinales/ProyectoFinal/ProyectoPSP/src/main/java/com/example/proyectopsp/LoginController.java
+++ TemasFinales/ProyectoFinal/ProyectoPSP/src/main/java/com/example/proyectopsp/LoginController.java
@@ -16,36 +16,15 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginController  implements Initializable {
-
-
     @FXML
     private Button boton_aceptar;
-
     @FXML
     private TextField texto_contrasenia;
-
     @FXML
     private TextField texto_nombre;
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        boton_aceptar.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("pantalla-principal.fxml"));
-                try {
-                    Parent root = loader.load();
-                    Scene scene = new Scene(root,600,400);
-                    Stage stage = new Stage();
-                    stage.setScene(scene);
-                    PrincipalController controller = loader.getController();
-                    controller.setController(LoginController.this);
-                    stage.show();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
+        
     }
 
 
