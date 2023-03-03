bd49eb375c771af9ea78f501fd41e99573112196
AlexAguado24 <aaguado131@gmail.com>
2023-03-03 14:10:38 +0100

    .

--- UltimoTrabajo/app/src/main/java/com/example/ultimotrabajo/PrincipalActivity.kt
+++ UltimoTrabajo/app/src/main/java/com/example/ultimotrabajo/PrincipalActivity.kt
@@ -4,26 +4,42 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.ultimotrabajo.databinding.ActivityPrincipalBinding
+import com.example.ultimotrabajo.objetos.Zapatillas
 
 
 class PrincipalActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPrincipalBinding
+    private lateinit var listaZapas: ArrayList<Zapatillas>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPrincipalBinding.inflate(layoutInflater)
         setContentView(binding.root)
         butonActions()
+        instancias();
+    }
+
+    private fun instancias() {
+        listaZapas = ArrayList();
+        listaZapas.add(Zapatillas("Forum","Adidas",R.drawable.adidas_forum))
+        listaZapas.add(Zapatillas("Samba","Adidas",R.drawable.samba))
+        listaZapas.add(Zapatillas("Green","Adidas",R.drawable.adiddas_verde))
+        listaZapas.add(Zapatillas("Air Force One","Nike",R.drawable.air_force))
+        listaZapas.add(Zapatillas("Air Jordan","Nike",R.drawable.air_jordan))
+        listaZapas.add(Zapatillas("Air Max","Nike",R.drawable.air_max))
+        listaZapas.add(Zapatillas("Smash 2","Puma",R.drawable.puma_smash2))
+        listaZapas.add(Zapatillas("Ferrari","Puma",R.drawable.puma_ferrari))
+        listaZapas.add(Zapatillas("California","Puma",R.drawable.puma_cali))
+        listaZapas.add(Zapatillas("Manteca","DC",R.drawable.dc_manteca))
+        listaZapas.add(Zapatillas("Red DC","DC",R.drawable.dc_rojas))
+        listaZapas.add(Zapatillas("Vandium","DC",R.drawable.dc_vandium))
     }
 
     private fun butonActions() {
         binding.botonAtras.setOnClickListener{
-            val intent= Intent(this,MainActivity::class.java)
-            startActivity(intent)
-            finish()
+
         }
     }
-
-
 }
---
+++ UltimoTrabajo/app/src/main/java/com/example/ultimotrabajo/objetos/Zapatillas.kt
@@ -0,0 +1,3 @@
+package com.example.ultimotrabajo.objetos
+
+data class Zapatillas(var nombre: String, var tipo:String, var imagen:Int):java.io.Serializable
